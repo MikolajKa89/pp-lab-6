64f7a0d4d627807aa99cc9d159149a7a4a91938a
MikolajKa <miki@radzymin.com.pl>
2024-05-05 12:41:57 +0200

    dodoaj

--- Main.java
+++ Main.java
@@ -1,4 +1,3 @@
-package company;
 
 import company.abstracts.Employee;
 import company.models.Manager;
@@ -9,24 +8,75 @@ import java.util.List;
 
 public class Main {
 
+    private static Employee worker1;
+    private static Employee worker2;
+    private static Employee worker4;
+    private static Employee worker3;
+
     public static void main(String[] args) {
-        Worker worker1 = new Worker("Jan Kowalski", 2500, 1, "2023-01-01", "Software Engineer");
-        Worker worker2 = new Worker("Anna Nowak", 3000, 2, "2022-06-01", "Data Analyst");
-        Worker worker3 = new Worker("Piotr Zieliński", 2200, 3, "2024-03-01", "Junior Developer");
-        Worker worker4 = new Worker("Marta Wiśniewska", 2800, 4, "2023-05-01", "UX/UI Designer");
+        // ... (Utworzenie instancji obiektów)
 
-        Manager manager = new Manager("Krzysztof Baran", 5000, 5, "2020-02-01", "CTO");
-
+        // Dodanie instancji do listy
         List<Employee> employees = new ArrayList<>();
         employees.add(worker1);
         employees.add(worker2);
         employees.add(worker3);
         employees.add(worker4);
+        Employee manager;
         employees.add(manager);
 
+        // Zliczanie sum pensji
+        double totalSalary = 0;
+        double totalManagerSalary = 0;
+        double totalWorkerSalary = 0;
+
         for (Employee employee : employees) {
-            System.out.println("- " + employee.getName() + " (ID: " + employee.getId() + ", Position: " + employee.getPosition() + ", Hire Date: " + employee.getHireDate() + ")");
-            employee.work();
+            totalSalary += employee.getSalary();
+            if (employee instanceof Manager) {
+                totalManagerSalary += employee.getSalary();
+            } else if (employee instanceof Worker) {
+                totalWorkerSalary += employee.getSalary();
+            }
+        }
+
+        // Wyświetlanie sum pensji
+        System.out.println("\nCałkowita suma pensji wszystkich pracowników: " + totalSalary);
+        System.out.println("Całkowita suma pensji wszystkich managerów: " + totalManagerSalary);
+        System.out.println("Całkowita suma pensji wszystkich pracowników fizycznych: " + totalWorkerSalary);
+
+        // Wyszukiwanie obiektów z powtarzającym się ID
+        System.out.println("\nWyszukiwanie obiektów z powtarzającym się ID:");
+
+        for (Employee employee : employees) {
+            for (Employee employeeToCompare : employees) {
+                if (employee.getId() == employeeToCompare.getId() && !employee.equals(employeeToCompare)) {
+                    System.out.println("Znaleziono powtarzające się ID: " + employee.getId());
+                    System.out.println("Obiekt 1: " + employee);
+                    System.out.println("Obiekt 2: " + employeeToCompare);
+                    System.out.println();
+                }
+            }
+        }
+
+        // Dodanie kolejnych instancji z powtarzającym się ID
+        Worker worker5 = new Worker("Grzegorz Wiśniewski", 3500, 1, "2022-08-01", "Software Developer", null); // Powtarza ID z worker1
+        Manager manager2 = new Manager("Anna Kowalska", 6000, 5, "2021-01-01", "CEO"); // Powtarza ID z manager
+
+        employees.add(worker5);
+        employees.add(manager2);
+
+        // Ponowne wyszukiwanie obiektów z powtarzającym się ID
+        System.out.println("\nPonowne wyszukiwanie obiektów z powtarzającym się ID:");
+
+        for (Employee employee : employees) {
+            for (Employee employeeToCompare : employees) {
+                if (employee.getId() == employeeToCompare.getId() && !employee.equals(employeeToCompare)) {
+                    System.out.println("Znaleziono powtarzające się ID: " + employee.getId());
+                    System.out.println("Obiekt 1: " + employee);
+                    System.out.println("Obiekt 2: " + employeeToCompare);
+                    System.out.println();
+                }
+            }
         }
     }
 }
