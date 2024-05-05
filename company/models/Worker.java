b32caf0fc1716475338d04da6b3ed51433847bfa
MikolajKa89 <164363596+MikolajKa89@users.noreply.github.com>
2024-05-05 13:14:13 +0200

    Update Worker.java

--- company/models/Worker.java
+++ company/models/Worker.java
@@ -1,20 +1,14 @@
+// Worker.java
 package company.models;
-
 import company.abstracts.Employee;
 
 public final class Worker extends Employee {
-
-    public Worker(String employeeName, double annualSalary, int employeeId) {
-        super(employeeName, annualSalary, employeeId);
+    public Worker(String name, double salary, int id) {
+        super(name, salary, id);
     }
 
     @Override
-    public void performTask() {
-        System.out.println("Worker " + getName() + " is working...");
-    }
-
     public void work() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'work'");
+        System.out.println("Worker " + getName() + " is working...");
     }
 }
